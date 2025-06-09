ce126479368fb64e448aa6a609497d755436fff2
Vickyboretto <110607657+Vickyboretto@users.noreply.github.com>
2025-06-09 15:42:54 -0300

    se suben los archivos .c y .h del sensor de temperatura y humedad

---
+++ firmware/projects/examen/main/examen.c
@@ -0,0 +1,39 @@
+/*! @mainpage Examen 09/06
+ *
+ * @section se resuelve el examen
+ *
+ * This section describes how the program works.
+ *
+ * 
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 09/06/2025 | Document creation		                         |
+ *
+ * @author Maria Victoria Boretto (vickybore00@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
